ae27c44a4efe0b95de12a36e481045834f5864e7
MarcosDev <marcos.dev038@gmail.com>
2025-04-21 17:09:06 -0300

    inicialização do projeto

---
+++ CadastroDeAlunos/src/main/java/BancoDeDados/ConfigDB.java
@@ -0,0 +1,23 @@
+package BancoDeDados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConfigDB {
+
+	
+	/*Configuração para não ficar fazendo isso em outras parte do código.*/
+	public static Connection DadosDB() {
+		String url = "jdbc:mysql://localhost:3306/projeto_financeiro";
+		String usuario = "root";
+		String senha = "";
+		try {
+			Connection conexao = DriverManager.getConnection(url, usuario, senha);
+			return conexao;
+		} catch (Exception e) {
+			 System.out.println("Erro na class ConfigDB" + e.getMessage());
+			return null;
+		}
+		
+	}
+}
